0a5fd7ea51d5f20cf9bb58e4108a478789c1d7de
iolloyd <lloyd@codegood.co>
2017-06-07 06:07:55 -0400

    wip

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 import redis
-from lib.engine import add_match, get_outfit_for
-
-def init_redis(host='localhost', port=6379, db=0):
-    r = redis.Redis(host=host, port=port, db=db)
-    r.flushdb()
-    return r
+from lib.redis_store import add_match, get_outfit_for
+from lib.db import get_labels as get_db_labels, get_tags
 
 def load_items(r, items_labels):
     """An item is structured like:
@@ -15,7 +11,8 @@ def load_items(r, items_labels):
         r.sadd('labels:{}'.format(labels), key)
 
 def load_matches(r, matches):
-    [add_match(r, key, matches) for key, matches in matches.items()]
+    [add_match(r, key, matches) 
+     for key, matches in matches.items()]
 
 items = {
     1: 'a:b:c', 2: 'a:c:e', 3: 'a:b:e', 4: 'd:e:f',
@@ -23,23 +20,6 @@ items = {
 }
 
 matches = {
-    'a:b:c': ['a:b:e', 'd:e:f'],
-    'a:c:e': ['d:e:g'],
-    'a:b:e': ['g:j:k']
+    'top': ['bottom',],
+    'jeans:high': ['top:cropped',]
 }
-
-r = init_redis()
-load_items(r, items)
-load_matches(r, matches)
-
-actual = set(get_outfit_for(r, 1))
-expected = {'3', '4', '8'}
-assert actual == expected
-
-actual = set(get_outfit_for(r, 2))
-expected = {'5'}
-assert actual == expected
-
-actual = set(get_outfit_for(r, 6))
-expected = {'3', '8'}
-assert actual == expected
