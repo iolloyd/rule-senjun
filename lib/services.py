a2958cb43ee8c331c536826f30e9340d102fbf5e
iolloyd <lloyd@codegood.co>
2017-06-21 11:50:37 +0100

    adds outfits method and reformats the code

--- lib/services.py
+++ lib/services.py
@@ -1,13 +1,21 @@
+import json
 from bottle import route, run, response
 from os.path import basename, dirname, realpath
-from db import get_basics, get_items, get_items_labels, get_images
-from redis_store import get_outfit_items
-import json
+from redis_store import get_outfit_items, get_outfits
+from db import (get_basics, 
+                get_items, 
+                get_items_labels, 
+                get_images
+                )
+
+
+def items_with_images(id):
+    items = get_outfit_items(id)
+    items_with_images = get_images(items)
 
 
 def items(id):
-    items = get_outfit_items(id)
-    items_with_images = get_images(items)
+    items = items_with_images(id)
 
     return json.dumps(items_with_images)
 
@@ -16,4 +24,8 @@ def basics():
     items = [(x['id'], x['imageName']) for x in get_basics()]
     return json.dumps(items)
 
+
+def outfits(id):
+    item_outfits = get_outfit_items(id)
+    return item_outfits[:10]
 
